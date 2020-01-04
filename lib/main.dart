ff6e1869ae68d6321d3351c78b02504f5b765eda
TinchoOlivari <martin.olivari01@gmail.com>
2020-01-04 15:49:48 -0300

    drawerScrimColor changed to white

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,7 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawerScrimColor: Colors.white70,
       body: _widgetOptions.elementAt(_selectedIndex),
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
