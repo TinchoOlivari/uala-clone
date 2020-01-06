87b63330717249e76770e4e909b97cb8f455c185
TinchoOlivari <martin.olivari01@gmail.com>
2020-01-06 00:13:24 -0300

    CustomListTile moved to 'parts' folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:uala_clone/screens/splash_screen/splash_screen.dart';
 import 'package:uala_clone/screens/home/home.dart';
+import 'package:uala_clone/parts/curstomListTile.dart';
 
 void main() => runApp(AppEntry());
 
@@ -279,75 +280,3 @@ class DrawerPainter extends CustomPainter {
   bool shouldRepaint(CustomPainter oldDelegate) => false;
 }
 
-class CustomListTile extends StatelessWidget {
-  CustomListTile(
-      {this.index, @required this.iconData, @required this.title, this.number});
-
-  final ValueNotifier<int> number;
-
-  final int index;
-  final IconData iconData;
-  final String title;
-
-  @override
-  Widget build(BuildContext context) {
-    if (index == number.value) {
-      return Material(
-        color: Colors.transparent,
-        child: InkWell(
-          highlightColor: Colors.white12,
-          splashColor: Colors.transparent,
-          onTap: () {},
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 17.5),
-            decoration: BoxDecoration(color: Colors.white24),
-            child: ListTile(
-              dense: true,
-              contentPadding: EdgeInsets.all(0),
-              title: Text(title,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.w400)),
-              leading: Icon(
-                iconData,
-                size: 25,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      );
-    } else {
-      return Material(
-        color: Colors.transparent,
-        child: InkWell(
-          highlightColor: Colors.white12,
-          onTap: () {
-            if (index != null){
-              number.value = index;
-            }
-            Navigator.pop(context);
-          },
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 17.5),
-            child: ListTile(
-              dense: true,
-              contentPadding: EdgeInsets.all(0),
-              title: Text(title,
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 17,
-                      fontWeight: FontWeight.w400)),
-              leading: Icon(
-                iconData,
-                size: 25,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ),
-      );
-    }
-  }
-}
