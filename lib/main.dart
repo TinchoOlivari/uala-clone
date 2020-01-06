83cf7e990ee21e84ed4df846db53d73cfa254c87
TinchoOlivari <martin.olivari01@gmail.com>
2020-01-06 00:09:17 -0300

    Big drawer navigation fixed

--- lib/main.dart
+++ lib/main.dart
@@ -30,194 +30,220 @@ class App extends StatefulWidget {
   _AppState createState() => _AppState();
 }
 
-int _selectedIndex = 0;
-
 class _AppState extends State<App> {
+  ValueNotifier<int> number = ValueNotifier(0);
+
   static List<Widget> _widgetOptions = <Widget>[
     MyHomePage(),
-    Text(
-      'Index 1: Business',
+    Center(
+      child: Text(
+        'Index 1',
+      ),
     ),
-    Text(
-      'Index 1: Business',
+    Center(
+      child: Text(
+        'Index 2',
+      ),
     ),
-    Text(
-      'Index 1: Business',
+    Center(
+      child: Text(
+        'Index 3',
+      ),
     ),
   ];
 
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      number.value = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawerScrimColor: Colors.white70,
-      body: _widgetOptions.elementAt(_selectedIndex),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            title: Text('Incio'),
+    return ValueListenableBuilder(
+      valueListenable: number,
+      builder: (context, value, child) {
+        return Scaffold(
+          drawerScrimColor: Colors.white70,
+          body: _widgetOptions.elementAt(value)!= null
+              ? _widgetOptions.elementAt(value)
+              : _widgetOptions.elementAt(0),
+          bottomNavigationBar: BottomNavigationBar(
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Incio'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.swap_vert),
+                title: Text('Transferencias'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.credit_card),
+                title: Text('Tarjeta'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.insert_drive_file),
+                title: Text('Pagos'),
+              ),
+            ],
+            currentIndex: value,
+            onTap: _onItemTapped,
+            showUnselectedLabels: true,
+            type: BottomNavigationBarType.fixed,
+            iconSize: 23,
+            selectedItemColor: Colors.blue,
+            unselectedIconTheme: IconThemeData(color: Colors.blue, size: 26),
+            selectedLabelStyle:
+                TextStyle(fontSize: 11, fontWeight: FontWeight.w700),
+            unselectedLabelStyle: TextStyle(fontSize: 12),
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.swap_vert),
-            title: Text('Transferencias'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.credit_card),
-            title: Text('Tarjeta'),
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.insert_drive_file),
-            title: Text('Pagos'),
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        showUnselectedLabels: true,
-        type: BottomNavigationBarType.fixed,
-        iconSize: 23,
-        selectedItemColor: Colors.blue,
-        unselectedIconTheme: IconThemeData(color: Colors.blue, size: 26),
-        selectedLabelStyle:
-            TextStyle(fontSize: 11, fontWeight: FontWeight.w700),
-        unselectedLabelStyle: TextStyle(fontSize: 12),
-      ),
-      drawer: SizedBox(
-        width: MediaQuery.of(context).size.width * 0.787,
-        child: Drawer(
-          child: CustomPaint(
-              painter: DrawerPainter(),
-              child: Column(
-                children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 25, horizontal: 17.5),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Stack(
-                          children: <Widget>[
-                            Positioned(
-                              child: IconButton(
-                                icon: Icon(
-                                  Icons.close,
-                                  color: Colors.white,
-                                  size: 20,
+          drawer: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.787,
+            child: Drawer(
+              child: CustomPaint(
+                painter: DrawerPainter(),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 25, horizontal: 17.5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Stack(
+                            children: <Widget>[
+                              Positioned(
+                                child: IconButton(
+                                  icon: Icon(
+                                    Icons.close,
+                                    color: Colors.white,
+                                    size: 20,
+                                  ),
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
                                 ),
-                                onPressed: () {
-                                  Navigator.pop(context);
-                                },
                               ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Padding(
-                              padding: const EdgeInsets.only(right: 25),
-                              child: CircleAvatar(
-                                  backgroundColor: Colors.white,
-                                  radius: 35,
-                                  child: Icon(
-                                    Icons.person,
-                                    size: 50,
-                                    color: Colors.grey,
-                                  )),
-                            ),
-                            Container(
-                              child: Text(
-                                'Martín Tomas Olivari Spada',
-                                style: TextStyle(
-                                    fontSize: 20, color: Colors.white),
-                              ),
-                              width: 200,
-                            )
-                          ],
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 95),
-                          child: RaisedButton(
-                            materialTapTargetSize:
-                                MaterialTapTargetSize.shrinkWrap,
-                            padding: EdgeInsets.symmetric(horizontal: 10),
-                            elevation: 0,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(18.0),
-                              side: BorderSide(color: Colors.white, width: 1.5),
-                            ),
-                            onPressed: () {},
-                            color: Colors.transparent,
-                            textColor: Colors.white,
-                            child: Text('Ver perfil',
-                                style: TextStyle(
-                                    fontSize: 16, fontWeight: FontWeight.w400)),
+                            ],
                           ),
-                        )
+                          Row(
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(right: 25),
+                                child: CircleAvatar(
+                                    backgroundColor: Colors.white,
+                                    radius: 35,
+                                    child: Icon(
+                                      Icons.person,
+                                      size: 50,
+                                      color: Colors.grey,
+                                    )),
+                              ),
+                              Container(
+                                child: Text(
+                                  'Martín Tomas Olivari Spada',
+                                  style: TextStyle(
+                                      fontSize: 20, color: Colors.white),
+                                ),
+                                width: 200,
+                              )
+                            ],
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 95),
+                            child: RaisedButton(
+                              materialTapTargetSize:
+                                  MaterialTapTargetSize.shrinkWrap,
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              elevation: 0,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                                side:
+                                    BorderSide(color: Colors.white, width: 1.5),
+                              ),
+                              onPressed: () {},
+                              color: Colors.transparent,
+                              textColor: Colors.white,
+                              child: Text('Ver perfil',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w400)),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: <Widget>[
+                        CustomListTile(
+                          title: 'Inicio',
+                          iconData: Icons.home,
+                          index: 0,
+                          number: number,
+                        ),
+                        CustomListTile(
+                          title: 'Transferencias',
+                          iconData: Icons.swap_vert,
+                          index: 1,
+                          number: number,
+                        ),
+                        CustomListTile(
+                          title: 'Tarjeta',
+                          iconData: Icons.credit_card,
+                          index: 2,
+                          number: number,
+                        ),
+                        CustomListTile(
+                          title: 'Pagos',
+                          iconData: Icons.insert_drive_file,
+                          index: 3,
+                          number: number,
+                        ),
+                        CustomListTile(
+                          title: 'Inversiones',
+                          iconData: Icons.timeline,
+                          number: number,
+                        ),
+                        CustomListTile(
+                          title: 'Analisis de gastos',
+                          iconData: Icons.pie_chart_outlined,
+                          number: number,
+                        ),
                       ],
                     ),
-                  ),
-                  Column(
-                    children: <Widget>[
-                      CustomListTile(
-                        title: 'Inicio',
-                        iconData: Icons.home,
-                        index: 0,
-                      ),
-                      CustomListTile(
-                        title: 'Transferencias',
-                        iconData: Icons.swap_vert,
-                        index: 1,
-                      ),
-                      CustomListTile(
-                        title: 'Tarjeta',
-                        iconData: Icons.credit_card,
-                        index: 2,
-                      ),
-                      CustomListTile(
-                        title: 'Pagos',
-                        iconData: Icons.insert_drive_file,
-                        index: 3,
-                      ),
-                      CustomListTile(
-                        title: 'Inversiones',
-                        iconData: Icons.timeline,
-                      ),
-                      CustomListTile(
-                        title: 'Analisis de gastos',
-                        iconData: Icons.pie_chart_outlined,
-                      ),
-                    ],
-                  ),
-                  Divider(
-                    color: Colors.white12,
-                    thickness: 1,
-                  ),
-                  CustomListTile(
-                    title: 'Invitar a un amigo',
-                    iconData: Icons.share,
-                  ),
-                  CustomListTile(
-                    title: 'Ayuda',
-                    iconData: Icons.help_outline,
-                  ),
-                  CustomListTile(
-                    title: 'Configuración',
-                    iconData: Icons.settings,
-                  ),
-                  CustomListTile(
-                    title: 'Cerrar',
-                    iconData: Icons.exit_to_app,
-                  ),
-                ],
-              )),
-        ),
-      ),
+                    Divider(
+                      color: Colors.white12,
+                      thickness: 1,
+                    ),
+                    CustomListTile(
+                      title: 'Invitar a un amigo',
+                      iconData: Icons.share,
+                      number: number,
+                    ),
+                    CustomListTile(
+                      title: 'Ayuda',
+                      iconData: Icons.help_outline,
+                      number: number,
+                    ),
+                    CustomListTile(
+                      title: 'Configuración',
+                      iconData: Icons.settings,
+                      number: number,
+                    ),
+                    CustomListTile(
+                      title: 'Cerrar',
+                      iconData: Icons.exit_to_app,
+                      number: number,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
@@ -254,7 +280,10 @@ class DrawerPainter extends CustomPainter {
 }
 
 class CustomListTile extends StatelessWidget {
-  CustomListTile({this.index, @required this.iconData, @required this.title});
+  CustomListTile(
+      {this.index, @required this.iconData, @required this.title, this.number});
+
+  final ValueNotifier<int> number;
 
   final int index;
   final IconData iconData;
@@ -262,7 +291,7 @@ class CustomListTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (index == _selectedIndex) {
+    if (index == number.value) {
       return Material(
         color: Colors.transparent,
         child: InkWell(
@@ -294,8 +323,12 @@ class CustomListTile extends StatelessWidget {
         color: Colors.transparent,
         child: InkWell(
           highlightColor: Colors.white12,
-          splashColor: Colors.transparent,
-          onTap: () {},
+          onTap: () {
+            if (index != null){
+              number.value = index;
+            }
+            Navigator.pop(context);
+          },
           child: Container(
             padding: const EdgeInsets.symmetric(horizontal: 17.5),
             child: ListTile(
