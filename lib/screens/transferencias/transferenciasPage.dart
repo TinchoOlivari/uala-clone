80fb9b58c86f091992b0fe607ce3c05cdb0b528e
TinchoOlivari <martin.olivari01@gmail.com>
2020-01-31 00:45:30 -0300

    Buttons "Nueva transferencia" and "Escanear QR"

--- lib/screens/transferencias/transferenciasPage.dart
+++ lib/screens/transferencias/transferenciasPage.dart
@@ -51,13 +51,80 @@ class _TransferenciasPageState extends State<TransferenciasPage> {
             child: Text(
               'Contactos Ualá',
               style:
-              TextStyle(color: Colors.black54, fontWeight: FontWeight.w600),
+                  TextStyle(color: Colors.black54, fontWeight: FontWeight.w600),
             ),
           ),
           Contacto(name: 'Ivan Luis Nuñez', date: '28/11/2019'),
           Contacto(name: 'Juan Cruz Mare', date: '18/11/2019'),
           Contacto(name: 'Fabrizio Agustin Olivari Spada', date: '05/10/2019'),
           Contacto(name: 'Elias Ivan Alaniz', date: '10/09/2019'),
+          Spacer(),
+          Padding(
+            padding: EdgeInsets.fromLTRB(20, 0, 20, 25),
+            child: Column(
+              children: <Widget>[
+                RaisedButton(
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.white54,
+                    focusElevation: 4,
+                    highlightElevation: 4,
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(35),
+                    ),
+                    onPressed: () {},
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 52,
+                      child: Center(
+                        child: Text(
+                          'Nueva transferencia',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.w400),
+                        ),
+                      ),
+                    )),
+                RaisedButton(
+                    materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                    splashColor: Colors.transparent,
+                    highlightColor: Colors.blue[180],
+                    focusElevation: 0,
+                    highlightElevation: 0,
+                    elevation: 0,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(35),
+                    ),
+                    onPressed: () {},
+                    color: Colors.transparent,
+                    textColor: Colors.blue,
+                    child: SizedBox(
+                      width: double.infinity,
+                      height: 52,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: Icon(
+                              Icons.photo_camera,
+                              color: Colors.blue,
+                              size: 25,
+                            ),
+                          ),
+                          Text(
+                            'Escanear QR',
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w400),
+                          ),
+                        ],
+                      ),
+                    )),
+              ],
+            ),
+          ),
         ],
       ),
     );
@@ -93,7 +160,9 @@ class ContactoState extends State<Contacto> {
                   child: CircleAvatar(
                     child: Text(
                       widget.name[0],
-                      style: TextStyle(color: Colors.white,),
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
                     ),
                     backgroundColor: const Color(0xFFFF686B),
                   ),
